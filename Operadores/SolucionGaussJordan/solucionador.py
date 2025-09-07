0f536e372e4ccfade1851630e7d1a7de71076318
andrea1234e <122126891+andrea1234e@users.noreply.github.com>
2025-09-06 23:57:35 -0600

    instalador

--- Operadores/SolucionGaussJordan/solucionador.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-from typing import Protocol
-from Operadores.sistema_lineal import SistemaLineal
-from .solucion import Solucion
-
-
-class Solucionador(Protocol):
-    def resolver(self, sistema: SistemaLineal, registrar_pasos: bool = False) -> Solucion:
-        ...
